02f3d54cde3d410afd9739e72e099fa092f60597
Witold Rugowski <rugowski@nhw.pl>
2015-03-06 12:20:01 +0100

    naprawiamy składnię pythona

--- pi-controller.py
+++ pi-controller.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 
-binaryCode = 
-	{	
+binaryCode = {	
 	"1": {"true": "010100000100010101010011", "false": "010100000100010101011100"},
 	"2": {"true": "010100000101000101010011", "false": "010100000101000101011100"},
 	"3": {"true": "010100000101010001010011", "false": "010100000101010001011100"},
@@ -11,8 +10,8 @@ binaryCode =
 
 node = os.popen('node yuncms.js')
 while True:
-    command = node.readline()
-    command = command.strip()
+	command = node.readline()
+	command = command.strip()
 	idx = command.split()[0]
 	action = command.split()[1]
 	str = binaryCode[idx][action]
